675178d8f67c3ce9e4ce95a4cc168c954ff92220
joaocastro <joaocastro0429@gmail.com>
2024-04-04 09:11:23 -0300

    update

--- index.js
+++ index.js
@@ -4,11 +4,42 @@ const express = require('express')
 
 const port=process.env.PORT || 3333
 const app=express()
+ 
 
-
-
+app.use(express.json())
 app.get("/",(request,response)=>{
-    return  response.send("mergulho nodejs")
+    return  response.json({msg:"mergurlho nodejs"})
+})
+
+app.post("/task",(request,response)=>{
+
+    const data=request.body
+
+    response.status(201).json({data})
+
+})
+
+app.get("/task",(request,response)=>{
+  return response.status(200).json({msg:"listando uma tarefa"})
+})
+app.get("/task/:id",(request,response)=>{
+    const {id}=request.params
+
+    return response.status(200).json({msg:`listando a tarefa ${id}`})
+
+})
+app.put("/task/:id",(request,response)=>{
+    const {id}=request.params
+
+    return response.status(200).json({msg:`atualizando a tarefa ${id}`})
+
+})
+
+app.delete("/task/:id",(request,response)=>{
+    const {id}=request.params
+
+    return response.status(200).json({msg:`deletando  a tarefa ${id}`})
+
 })
 
 
